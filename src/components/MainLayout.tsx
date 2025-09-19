88555e28092fc1b29cbacadbcacab8e05294541c
wonmo <129805449+9Gu-Mo@users.noreply.github.com>
2025-09-19 12:55:21 +0900

    chore: main 영역 수정

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -10,7 +10,7 @@ export default function MainLayout({
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main>{children}</main>
+      <main className="flex-1">{children}</main>
       <Footer />
     </div>
   );
